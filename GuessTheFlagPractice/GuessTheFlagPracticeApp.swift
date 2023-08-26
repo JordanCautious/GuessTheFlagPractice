5592692ea2b735ed0c2579c7f05e27198b06b65c
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2023-08-26 04:23:01 -0400

    Initial Commit

---
+++ GuessTheFlagPractice/GuessTheFlagPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagPracticeApp.swift
+//  GuessTheFlagPractice
+//
+//  Created by Jordan Haynes on 8/26/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
